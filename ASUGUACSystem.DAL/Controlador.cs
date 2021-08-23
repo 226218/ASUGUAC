bdce89758be4b20efc9b41b945b41345573b4a62
226218 <takato_751@hotmail.com>
2021-08-23 10:41:40 -0500

    Add files via upload

---
+++ ASUGUACSystem.DAL/Controlador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASUGUACSystem.DAL
+{
+    public class Controlador
+    {
+        public EscuelaProfesional SeleccionarEscuelaProfesional(string pIDEscuelaProfesional)
+        {
+            var db = new ASUGUACEntities();
+            return db.EscuelaProfesional.Find(pIDEscuelaProfesional);
+        }
+        public List<EscuelaProfesional> SeleccionarEscuelaProfesionals()
+        {
+            var db = new ASUGUACEntities();
+            return db.EscuelaProfesional.ToList<EscuelaProfesional>();
+
+        }
+    }
+}
